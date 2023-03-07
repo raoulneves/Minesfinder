0eb69d859f98428504656a77caad25fb306e6650
Raoul Neves <raoulneves@gmail.com>
2023-03-07 11:40:24 +0000

    janela com tm min

--- src/main/java/pt/ipleiria/estg/dei/ei/esoft/MinesFinder.java
+++ src/main/java/pt/ipleiria/estg/dei/ei/esoft/MinesFinder.java
@@ -1,4 +1,21 @@
 package pt.ipleiria.estg.dei.ei.esoft;
 
-public class MinesFinder {
+import javax.swing.*;
+
+public class MinesFinder extends JFrame {
+
+    private JPanel primaryPanel;
+
+    public MinesFinder(String title) {
+        super(title);
+
+        setContentPane(primaryPanel);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        pack();
+    }
+
+    public static void main(String[] args) {
+        new MinesFinder("Mines Finder").setVisible(true);
+    }
 }
